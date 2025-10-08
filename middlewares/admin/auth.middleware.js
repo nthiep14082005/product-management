d4c610898b0b33e79f8a7961c70a44eabf03aab1
Nguyen The Hiep <169110383+nthiep14082005@users.noreply.github.com>
2025-10-08 15:35:03 +0700

    // app.locals.userAdmin = user; // -> không sử dụng được từ app bởi vì trong này ko có định nghĩa app mà trong index.js đầu thì dùng được bởi đã định nghĩa nhưng              res.locals.userAdmin = user; // -> có thể sử dụng được res để tạo biến toàn cục sử dụng trực tiếp trong file pug bởi vì tham số thứ 2 là res -> tức là phản hồi, mà phản hồi thì sẽ trả về được

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -4,7 +4,7 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../model/accounts.model");
 
-module.exports.requireAuth = async (req, res, next) => {
+module.exports.requireAuth = async (req, res, next) => { // Bạn có thể đặt tên tham số tùy ý (ví dụ: required, responsed, tiếp), nhưng thứ tự phải đúng: (request, response, next).
     
     // console.log("Chay qua day ");
 
@@ -22,6 +22,10 @@ module.exports.requireAuth = async (req, res, next) => {
             res.redirect(`${systemConfig.prefixAdmin}/auths/login`);
         } else {     
             // console.log(user);
+
+            // app.locals.userAdmin = user; // -> không sử dụng được từ app bởi vì trong này ko có định nghĩa app mà trong index.js đầu thì dùng được bởi đã định nghĩa nhưng 
+            res.locals.userAdmin = user; // -> có thể sử dụng được res để tạo biến toàn cục sử dụng trực tiếp trong file pug bởi vì tham số thứ 2 là res -> tức là phản hồi, mà phản hồi thì sẽ trả về được 
+
             next();
         }
     }
